e8fc861bcb64e76f343da3b9b5c87b99cdcac6cd
League Student <league@iMac-21.attlocal.net>
2019-11-02 11:29:18 -0700

    Happy pet unfinished
    :dragon: :

--- src/_08_string_methods/StringMethods.java
+++ src/_08_string_methods/StringMethods.java
@@ -8,16 +8,28 @@ public class StringMethods {
 	public static void main(String[] args) {
 		
 		// 1. Create a String variable and initialize it to whatever you want
+String test = "saed";
 
 		// 2. Print the 3rd char of your String to the console.
 		//    HINT: .charAt
+
+		for(int i = 0; i<test.length(); i+=1) {
+
+	System.out.println(test.charAt(i));
+	if( 'a' == test.charAt(i)) {
+		System.out.println("a is at index "+i);
+	}
+	else if( 's' == test.charAt(i)) {
+		System.out.println("s is at index "+i);
+	}
+	else if( 'e' == test.charAt(i)) {
+		System.out.println("e is at index "+i);
+	}
+	else {
+		System.out.println("d is at index "+i);
+	}
 		
-		// 3. Print the length of your String to the console.
-		//    HINT: .length()
-
-		// 4. Using a for loop, print one char at time to the console.
-		//    HINT: use .length() to determine how many loops
-
+		}
 		
 		// 5. Pick a char inside your String, and use a loop to determine
 		//    what position/index in the String the char is located.
